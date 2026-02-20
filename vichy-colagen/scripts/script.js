4319917e0589a7aa4d2ad931815e20eab0ba8435
WitchDevelops <d.n.wojewska@gmail.com>
2026-02-20 12:42:31 +0100

    fix: QA corrections

--- vichy-colagen/scripts/script.js
+++ vichy-colagen/scripts/script.js
@@ -1,14 +1,20 @@
-const swiper = new Swiper(".es-swiper", {
+
+
+(() => {
+  const root = document.querySelector("#es-rc #es-rc-content");
+  if (!root) return;
+  const swiper = new Swiper(root.querySelector(".es-swiper"), {
   direction: "horizontal",
   loop: true,
   autoHeight: true,
 
   pagination: {
-    el: ".es-swiper-pagination",
+    el: root.querySelector(".es-swiper-pagination"),
   },
 
   navigation: {
-    nextEl: ".es-swiper-button-next",
-    prevEl: ".es-swiper-button-prev",
+    nextEl: root.querySelector(".es-swiper-button-next"),
+    prevEl: root.querySelector(".es-swiper-button-prev"),
   },
 });
+})();
